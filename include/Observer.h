940612689371f6b8c1914b27be85d1bc19833943
Robert Stevens <robert@stevens.tk>
2015-06-01 23:50:32 +0200

    made some small changes and added the gitignore

--- include/Observer.h
+++ include/Observer.h
@@ -1,39 +1,38 @@
 #ifndef OBSERER_H_
 #define OBSERVER_H_
+#include <ctime>
 
 /**
  *Author: ing. R.J.H.M. Stevens
  *
  */
-class Observer{
-public:
-	virtual ~Observer();
-	virtual void onNotify(const ObserverHeader* header, const void* data);
-}
-
-class Subject{
-protected:
-	void notify(const ObserverHeader& header, const void* data);
-} 
-
-class Relay(){
-public: 
-	void registerSubject(Subject& subject, char[] channel);
-	void registerObserver(Observer& observer, char[] channel);
-}
-
-class ObserverHeader{
-	char[] channelName;
-	char[] sender;
-	date created;
-	unsigned int dataSize;
-	ObserverDataType type;
-}
-
 enum DataType{
 	CHAR,
 	INT,
 	FLOAT
 };
+class ObserverHeader{
+	char channelName[20];
+	char sender[20];
+	time_t created; //time(0);
+	unsigned int dataSize;
+	DataType type;
+};
 
+class Observer{
+public:
+	virtual ~Observer();
+	virtual void onNotify(const ObserverHeader* header, const void* data);
+};
+
+class Subject{
+protected:
+	void notify(const ObserverHeader& header, const void* data);
+};
+
+class Relay{
+public: 
+	void registerSubject(Subject& subject, const char* channel);
+	void registerObserver(Observer& observer, const char* channel);
+};
 #endif
