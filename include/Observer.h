5b7ef0876099b0ea85561f2157cfe028b8b66456
Robert Stevens <robert@stevens.tk>
2015-06-02 00:44:58 +0200

    made the classes interfaces

--- include/Observer.h
+++ include/Observer.h
@@ -1,38 +1,87 @@
+/**************************************************************************************\
+									Observer.h
+Author		:	ing.	R.J.H.M.	Stevens
+Begin		:	02-06-2015
+				
+Version		:	1.0.0
+TODO		:	
+Description	:	The file contains the interfaces of all classes that have somthing to do
+                     with the observer
+Dependencies:	RSLogger
+Changes		:
+\**************************************************************************************/
 #ifndef OBSERER_H_
 #define OBSERVER_H_
 #include <ctime>
 
+
 /**
- *Author: ing. R.J.H.M. Stevens
- *
- */
+*
+*/
 enum DataType{
 	CHAR,
 	INT,
 	FLOAT
 };
+/**
+*
+*/
 class ObserverHeader{
+public:
 	char channelName[20];
 	char sender[20];
 	time_t created; //time(0);
 	unsigned int dataSize;
 	DataType type;
 };
-
+/**
+ *
+ */
 class Observer{
 public:
-	virtual ~Observer();
-	virtual void onNotify(const ObserverHeader* header, const void* data);
-};
+	/**
+     *	@param
+	 */
+	virtual void onNotify(const ObserverHeader* header, const void* data) = 0;
 
+	/**
+	 *
+	 */
+	virtual ~Observer(){}
+};
+/**
+ *
+ */
 class Subject{
 protected:
-	void notify(const ObserverHeader& header, const void* data);
-};
+	/**
+     *	@param
+	 */
+	virtual void notify(const ObserverHeader& header, const void* data) = 0;
 
+	/**
+	 *
+	 */
+	virtual ~Subject(){}
+};
+/**
+ *
+ */
 class Relay{
 public: 
-	void registerSubject(Subject& subject, const char* channel);
-	void registerObserver(Observer& observer, const char* channel);
+	/**
+     *	@param
+	 */
+	virtual void registerSubject(Subject& subject, const char* channel) = 0;
+
+	/**
+     *	@param
+	 */
+	virtual void registerObserver(Observer& observer, const char* channel) = 0;
+
+	/**
+	 *
+	 */
+	virtual ~Relay(){}
 };
 #endif
