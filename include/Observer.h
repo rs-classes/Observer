b133ba165f261863fc0142d7c52822d3def96e4d
Robert Stevens <robert@stevens.tk>
2015-06-02 01:48:08 +0200

    added comments

--- include/Observer.h
+++ include/Observer.h
@@ -1,8 +1,8 @@
-/**************************************************************************************\
-									Observer.h
+/**************************************************************************************	\
+                                        Observer.h
 Author		:	ing.	R.J.H.M.	Stevens
 Begin		:	02-06-2015
-				
+			
 Version		:	1.0.0
 TODO		:	
 Description	:	The file contains the interfaces of all classes that have somthing to do
@@ -10,78 +10,72 @@ Description	:	The file contains the interfaces of all classes that have somthing
 Dependencies:	RSLogger
 Changes		:
 \**************************************************************************************/
-#ifndef OBSERER_H_
+#ifndef OBSERVER_H_
 #define OBSERVER_H_
 #include <ctime>
 
 
-/**
-*
-*/
-enum DataType{
-	CHAR,
-	INT,
-	FLOAT
-};
-/**
-*
-*/
 class ObserverHeader{
-public:
-	char channelName[20];
-	char sender[20];
-	time_t created; //time(0);
-	unsigned int dataSize;
-	DataType type;
+ public:
+  char channelName[254];
+  char sender[254];
+  time_t created; //time(0);
+  unsigned int dataSize;
+  char dataType[20];
 };
+
 /**
- *
+ *  The observer recives the messages from the subject
  */
 class Observer{
-public:
-	/**
-     *	@param
-	 */
-	virtual void onNotify(const ObserverHeader* header, const void* data) = 0;
+ public:
+  /**
+   *  Recives the notifications from the subjet
+   *
+   *  @param header The header with the basic info
+   *  @param data the real data
+   */
+  virtual void onNotify(const ObserverHeader* header, const void* data) = 0;
 
-	/**
-	 *
-	 */
-	virtual ~Observer(){}
+  virtual ~Observer(){}
 };
+
 /**
- *
+ *  This class creates the messages
  */
 class Subject{
-protected:
-	/**
-     *	@param
-	 */
-	virtual void notify(const ObserverHeader& header, const void* data) = 0;
-
-	/**
-	 *
-	 */
-	virtual ~Subject(){}
+ protected:
+  /**
+   *  Send the message to the observers
+   *  @param header the basic header
+   *  @param data the data that has to be sended
+   */
+  virtual void notify(const ObserverHeader& header, const void* data) = 0;
+  
+  virtual ~Subject(){}
 };
+
 /**
- *
+ *  This class sits between the subject and the observer, so there is no direct link between the observer and the subject
  */
 class Relay{
-public: 
-	/**
-     *	@param
-	 */
-	virtual void registerSubject(Subject& subject, const char* channel) = 0;
+ public: 
+  /**
+   *  Register a new subject at the rely
+   *
+   *  @param subject the subject
+   *  @param channel the channel on witch the subject wants to send
+   */
+  virtual void registerSubject(Subject& subject, const char* channel) = 0;
+  
+  /**
+   *  Register a new observer at the relay
+   *
+   *  @param observer the observer
+   *  @param channel the channel on witch the subject wants to send
+   */
+  virtual void registerObserver(Observer& observer, const char* channel) = 0;
 
-	/**
-     *	@param
-	 */
-	virtual void registerObserver(Observer& observer, const char* channel) = 0;
-
-	/**
-	 *
-	 */
-	virtual ~Relay(){}
+  virtual ~Relay(){}
 };
 #endif
